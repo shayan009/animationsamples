3c54df61947035240af2f3e7e337f6e662d7d4fe
debasish_ghosh <shayan.deba0930@gmail.com>
2018-10-28 19:58:34 +0530

    shimmer activity added.

---
+++ app/src/main/java/com/demoslideupdown/shimmer/ShimmerActivity.java
@@ -0,0 +1,1 @@
+package com.demoslideupdown.shimmer;import android.support.v7.app.AppCompatActivity;import android.os.Bundle;import com.demoslideupdown.R;public class ShimmerActivity extends AppCompatActivity {    @Override    protected void onCreate (Bundle savedInstanceState) {        super.onCreate(savedInstanceState);        setContentView(R.layout.activity_shimmer);    }}
